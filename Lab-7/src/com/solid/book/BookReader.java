7d1f133e5b5eabc83c275545f039ae3061ec330e
KRITSANA-KMITL <kritsanawipankhet@icloud.com>
2022-03-18 23:39:49 +0700

    added LAB7-SRP&LSP Success

--- Lab-7/src/com/solid/book/BookReader.java
+++ Lab-7/src/com/solid/book/BookReader.java
@@ -3,9 +3,18 @@ package com.solid.book;
 
 import java.util.List;
 
-public class BookReader {
+public class BookReader implements IReader{
     public static void main(String[] args) {
+
+        BookReader bookReader = new BookReader();
+        bookReader.printToScreen();
+    }
+
+    @Override
+    public void printToScreen() {
         Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
-        book.printToScreen();
+        do {
+            System.out.println(book.getCurrentPage());
+        } while (book.turnToNextPage());
     }
 }
--- Lab-7/src/com/solid/book/Main.java
+++
@@ -1,8 +0,0 @@
-package com.solid.book;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
